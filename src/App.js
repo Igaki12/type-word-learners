413f78347e85aa9f8fc285dfae45cf1279a69822
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2022-08-03 10:39:20 +0900

    made title design

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ import {
   Stack,
   Checkbox,
   Divider,
+  FormControl,
+  FormLabel,
+  Switch,
+  FormHelperText,
+  Tabs,
+  TabList,
+  Tab,
+  InputGroup,
+  InputLeftElement,
+  Input,
 } from '@chakra-ui/react'
+import { Search2Icon } from '@chakra-ui/icons'
 import './App.css'
 
 function App() {
@@ -19,31 +30,74 @@ function App() {
         maxW={'sm'}
         // bgColor="white"
         className="blink"
-        mb={5}
       >
         <Image src={logo} alt="Dan Abramov" />
       </Box>
       <Box
         margin={'auto'}
         maxW={'sm'}
+        p="3"
+        textAlign={'center'}
+        borderRadius="lg"
+        border={'1px'}
+        mb="3"
+      >
+        <Text fontWeight={'bold'} fontSize="xl">
+          latest data: 08-03 08:47
+        </Text>
+        <Text fontSize={'md'}>Hard random 16/60</Text>
+        <Text fontSize={'md'}>GWL1 GWL2 GWL3 </Text>
+        <Text pr={'5'} fontSize={'md'} bgColor="gray" textAlign={'right'}>
+          ...continue?
+        </Text>
+      </Box>
+      <Box
+        margin={'auto'}
+        maxW={'sm'}
         textAlign={'left'}
+        mt="4"
+        mb="2"
+        pl={'5'}
         // bgColor="white"
       >
-        <Button colorScheme="white" variant="ghost" fontSize={'3xl'}>
-          {'>'}Practice
-        </Button>
-        <Button colorScheme="white" variant="ghost" fontSize={'3xl'}>
-          {'>'}Easy for smartphone
-        </Button>
         <Button
-          colorScheme="white"
+          colorScheme="gray"
           variant="ghost"
           fontSize={'3xl'}
-          color="purple.200"
+          pl="1"
+          borderRadius={'sm'}
+          autoFocus
         >
-          {'>'}Hard for PC
+          {'>'}Practice
+        </Button>
+        <Button
+          colorScheme="gray"
+          variant="ghost"
+          fontSize={'3xl'}
+          pl="1"
+          borderRadius={'sm'}
+        >
+          {'>'}Easy {'('}for smartphone{')'}
+        </Button>
+        <Button
+          colorScheme="gray"
+          variant="ghost"
+          fontSize={'3xl'}
+          pl="1"
+          borderRadius={'sm'}
+        >
+          {'>'}Hard {'('}for PC{')'}
         </Button>
       </Box>
+      <Box margin={'auto'} maxW={'sm'} textAlign={'center'}>
+        <Tabs variant="soft-rounded" colorScheme="gray" m={1}>
+          <TabList>
+            <Tab fontSize="lg">ascend</Tab>
+            <Tab fontSize="lg">random</Tab>
+          </TabList>
+        </Tabs>
+      </Box>
+
       <Box
         margin={'auto'}
         maxW={'sm'}
@@ -53,14 +107,32 @@ function App() {
         pl="5"
         pt={'3'}
         pb="8"
-        mt={'5'}
-        mb="2"
+        mb="1"
       >
         <Stack spacing={[1]} direction={['column']}>
-          <Checkbox size={'lg'}>GWL1</Checkbox>
-          <Checkbox size={'lg'}>GWL2</Checkbox>
-          <Checkbox size={'lg'}>GWL3</Checkbox>
+          <Checkbox size={'lg'} colorScheme="gray">
+            GWL1
+          </Checkbox>
+          <Checkbox size={'lg'} colorScheme="gray">
+            GWL2
+          </Checkbox>
+          <Checkbox size={'lg'} colorScheme="gray">
+            GWL3
+          </Checkbox>
         </Stack>
+      </Box>
+      <Box margin={'auto'} maxW={'sm'} textAlign={'center'} mb="2">
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<Search2Icon color="gray.300" />}
+          />
+          <Input
+            type="text"
+            placeholder="Input keyword to search"
+            colorScheme={'blackAlpha'}
+          />
+        </InputGroup>
       </Box>
       {/* <Flex>
         <Box>
