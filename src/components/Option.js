fca9cf475c27c0af033c6e32362b2e7aedcb718a
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2022-08-05 10:51:29 +0900

    added good/bad buttons

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Button, Box, Text, Spacer, Progress } from '@chakra-ui/react'
 import {
+  CheckIcon,
   ChevronDownIcon,
   EditIcon,
+  RepeatIcon,
   SettingsIcon,
   TimeIcon,
 } from '@chakra-ui/icons'
@@ -24,7 +26,7 @@ export const Option = ({}) => {
           <SettingsIcon boxSize={'1em'} />
         </Button>
       </Box>
-      <Box w="200px" h="40px" top={'20px'} right={'60px'} position="fixed">
+      <Box w="150px" h="40px" top={'20px'} right={'60px'} position="fixed">
         <Progress colorScheme="gray" size="xs" value={20} />
         <Text textAlign={'right'} color="whiteAlpha.600">
           <EditIcon boxSize={'0.8em'} ml="2" />
@@ -33,17 +35,40 @@ export const Option = ({}) => {
           18:16
         </Text>
       </Box>
-      <Box w="60px" h="60px" bottom={3} right={3} position="fixed">
+      <Box bottom={'12px'} right={'12px'} position="fixed">
         <Button
           boxShadow={'dark-lg'}
           colorScheme="whiteAlpha"
-          size={'3xl'}
           borderRadius="sm"
           variant="solid"
           w={'60px'}
           h="60px"
         >
           <ChevronDownIcon boxSize={'2em'} />
+        </Button>
+      </Box>
+      <Box bottom={'88px'} right={'12px'} position="fixed">
+        <Button
+          boxShadow={'dark-lg'}
+          colorScheme="whiteAlpha"
+          borderRadius="sm"
+          variant="solid"
+          w={'40px'}
+          h="40px"
+        >
+          <RepeatIcon boxSize={'1.5em'} color="red.100" />
+        </Button>
+      </Box>
+      <Box bottom={'140px'} right={'12px'} position="fixed">
+        <Button
+          boxShadow={'dark-lg'}
+          colorScheme="whiteAlpha"
+          borderRadius="sm"
+          variant="solid"
+          w={'40px'}
+          h="40px"
+        >
+          <CheckIcon boxSize={'1.5em'} color="teal.100" />
         </Button>
       </Box>
     </>
